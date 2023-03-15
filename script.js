d6c9cdc16e41bcd019dc810f14ec0f54743f77e2
RuRuuuu <ayokooghenerukevwe2222@gmail.com>
2023-03-15 21:37:05 +0100

    my commit

---
+++ script.js
@@ -0,0 +1,165 @@
+
+
+window.onload = page;
+window.onresize = pgae;
+
+function page(){
+    myWidth = window.innerWidth;
+
+    
+
+}
+
+
+
+
+
+$("#num1").click(function(){
+    $("#num1").addClass("small");
+
+    setTimeout(function(){
+        $("#num1").removeClass("small")
+    }, 300);
+});
+
+$("#num2").click(function(){
+    $("#num2").addClass("small");
+
+    setTimeout(function(){
+        $("#num2").removeClass("small")
+    }, 300);
+});
+
+$("#num3").click(function(){
+    $("#num3").addClass("small");
+
+    setTimeout(function(){
+        $("#num3").removeClass("small")
+    }, 300);
+});
+
+$("#num4").click(function(){
+    $("#num4").addClass("small");
+
+    setTimeout(function(){
+        $("#num4").removeClass("small")
+    }, 300);
+});
+
+$("#num5").click(function(){
+    $("#num5").addClass("small");
+
+    setTimeout(function(){
+        $("#num5").removeClass("small")
+    }, 300);
+});
+
+$("#num6").click(function(){
+    $("#num6").addClass("small");
+
+    setTimeout(function(){
+        $("#num6").removeClass("small")
+    }, 300);
+});
+
+$("#num7").click(function(){
+    $("#num7").addClass("small");
+
+    setTimeout(function(){
+        $("#num7").removeClass("small")
+    }, 300);
+});
+
+$("#num8").click(function(){
+    $("#num8").addClass("small");
+
+    setTimeout(function(){
+        $("#num8").removeClass("small")
+    }, 300);
+});
+
+$("#num9").click(function(){
+    $("#num9").addClass("small");
+
+    setTimeout(function(){
+        $("#num9").removeClass("small")
+    }, 300);
+});
+
+$("#num10").click(function(){
+    $("#num10").addClass("small");
+
+    setTimeout(function(){
+        $("#num10").removeClass("small")
+    }, 300);
+});
+
+$("#num11").click(function(){
+    $("#num11").addClass("small");
+
+    setTimeout(function(){
+        $("#num11").removeClass("small")
+    }, 300);
+});
+
+$("#num12").click(function(){
+    $("#num12").addClass("small");
+
+    setTimeout(function(){
+        $("#num12").removeClass("small")
+    }, 300);
+});
+
+$("#num13").click(function(){
+    $("#num13").addClass("small");
+
+    setTimeout(function(){
+        $("#num13").removeClass("small")
+    }, 300);
+});
+
+$("#num14").click(function(){
+    $("#num14").addClass("small");
+
+    setTimeout(function(){
+        $("#num14").removeClass("small")
+    }, 300);
+});
+
+$("#num15").click(function(){
+    $("#num15").addClass("small");
+
+    setTimeout(function(){
+        $("#num15").removeClass("small")
+    }, 300);
+});
+
+    
+
+      $("#num17").click(function(){
+    $("#num17").addClass("small");
+
+    setTimeout(function(){
+        $("#num17").removeClass("small")
+    }, 300);
+});
+
+$("#num18").click(function(){
+    $("#num18").addClass("small");
+
+    setTimeout(function(){
+        $("#num18").removeClass("small")
+    }, 300);
+});
+
+
+
+$("#num16").on("click", function(){
+    $("#num16").addClass("small");
+
+    setTimeout(function(){
+        $("#num16").removeClass("small")
+    }, 300);
+})
+
+
